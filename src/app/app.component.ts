94f100fee408e3eb8c6905e08a9071f7a5a25e05
Rennay Dorasamy <rennayd@amazon.com>
2023-03-27 13:47:34 +0200

    S3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 
+import { Storage } from 'aws-amplify';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +9,38 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'amplify-monday';
+
+  async storage() {
+    console.log('Storage ***...');
+    const result = await Storage.put("test.txt", "Hello");
+    console.log(result);
+  }
+
+  // async onChange(e: any) {
+  //   console.log('onChange...');
+  //   const file = e.target.files[0];
+  //   try {
+  //     await Storage.put(file.name, file, {
+  //       contentType: "image/png", // contentType is optional
+  //     });
+  //   } catch (error) {
+  //     console.log("Error uploading file: ", error);
+  //   }
+  // }
+
+  fileToUpload: File | null = null;
+
+  // https://medium.com/better-programming/upload-files-in-ionic-angular-apps-784f0c382eb0
+  async handleFileInput(event: any) {
+    console.log('handleFileInput...');
+    console.log(event.target.files);
+    const file = event.target.files[0];
+    try {
+      await Storage.put(file.name, file, {
+        contentType: "image/png", // contentType is optional
+      });
+    } catch (error) {
+      console.log("Error uploading file: ", error);
+    }
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AmplifyAuthenticatorModule } from '@aws-amplify/ui-angular';
+import { Amplify } from "aws-amplify";
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
+import awsconfig from '../aws-exports';
+
+Amplify.configure(awsconfig);
 
 @NgModule({
   declarations: [
